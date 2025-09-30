45bbdc9c7149c96049c59709660557ad4de2d39e
Docmysh <bojames2001@gmail.com>
2025-09-30 13:30:20 -0400

    Shadow creatures fixed hurrah!

--- src/main/java/Vfx/vfx/event/ModEvents.java
+++ src/main/java/Vfx/vfx/event/ModEvents.java
@@ -3,9 +3,8 @@ package Vfx.vfx.event;
 import Vfx.vfx.Vfx;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,14 +13,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModEvents {
 
     @SubscribeEvent
-    public static void onAttributeCreate(EntityAttributeCreationEvent event) {
+    public static void onAttributeModify(EntityAttributeModificationEvent event) {
         for (EntityType<?> type : ForgeRegistries.ENTITY_TYPES) {
             if (LivingEntity.class.isAssignableFrom(type.getBaseClass())) {
                 @SuppressWarnings("unchecked")
                 EntityType<? extends LivingEntity> livingEntityType = (EntityType<? extends LivingEntity>) type;
-                AttributeSupplier.Builder builder = event.getAttributes().get(livingEntityType);
-                if (builder != null && !builder.hasAttribute(Attributes.ATTACK_DAMAGE)) {
-                    builder.add(Attributes.ATTACK_DAMAGE, 0.0D);
+                if (!event.has(livingEntityType, Attributes.ATTACK_DAMAGE)) {
+                    event.add(livingEntityType, Attributes.ATTACK_DAMAGE, 0.0D);
                 }
             }
         }
